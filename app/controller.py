9a9cb699bcf6f6d07579db4ce65d288c7f95bc32
Pablohsk <pablofidelisdias@gmail.com>
2024-02-09 11:50:51 -0300

    Class UserController Add

--- app/controller.py
+++ app/controller.py
@@ -4,3 +4,8 @@ class CarController:
     def create_car(self, modelo, ano, preco, tabela_fipe, kilometragem, utilitario):
         car = Car(modelo, ano, preco, tabela_fipe, kilometragem, utilitario)
         return car
+
+class UserController:
+    def create_user(self, nome, email, cpf):
+        user = User(nome, email, cpf)
+        return user
