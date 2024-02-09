c3ca0ce9b8365e79c6bb7007c4d183718d30d767
Pablohsk <pablofidelisdias@gmail.com>
2024-02-09 11:37:39 -0300

    Create __init__ class

---
+++ app/model.py
@@ -0,0 +1,8 @@
+class Car:
+    def __init__(self, modelo, ano, preco, tabela_fipe, kilometragem, utilitario):
+        self.modelo = modelo
+        self.ano = ano
+        self.preco = preco
+        self.tabela_fipe = tabela_fipe
+        self.kilometragem = kilometragem
+        self.utilitario = utilitario
