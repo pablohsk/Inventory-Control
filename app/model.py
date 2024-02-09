b9666af3c7b741d4c421b0c015714f74a687f970
Pablohsk <pablofidelisdias@gmail.com>
2024-02-09 13:48:09 -0300

    change in model Employee class

--- app/model.py
+++ app/model.py
@@ -15,14 +15,13 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     cpf = db.Column(db.String(14), unique=True, nullable=False)
 
-class Employee:
-    def __init__(self, login, nome, cpf, senha, nivel_atendimento):
-        self.login = login
-        self.nome = nome
-        self.cpf = cpf
-        self.senha = senha
-        self.nivel_atendimento = nivel_atendimento
-
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), unique=True, nullable=False)
+    nome = db.Column(db.String(100), nullable=False)
+    cpf = db.Column(db.String(14), unique=True, nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
+    nivel_atendimento = db.Column(db.Integer, nullable=False)
 
 class Sale:
     def __init__(self, car, user, employee, payment_method):
