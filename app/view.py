7cf4cb0d8dca2d5a79fde1b1fd9ee8581d70fd5e
Pablohsk <pablofidelisdias@gmail.com>
2024-02-09 13:38:17 -0300

    Class UserView Add

--- app/view.py
+++ app/view.py
@@ -1,3 +1,7 @@
 class CarView:
     def display_car_info(self, car):
         print(f"Car: {car.ano} {car.modelo}, Price: {car.preco}")
+
+class UserView:
+    def display_user_info(self, user):
+        print(f"User: {user.nome}, Email: {user.email}")
